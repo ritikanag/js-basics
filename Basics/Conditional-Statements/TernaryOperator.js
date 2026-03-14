d34a86662b509976e91d3274a2b27bda6c0faaf6
Ritika Nag <ritikanag@gmail.com>
2026-03-15 00:07:23 +0530

    update

---
+++ Basics/Conditional-Statements/TernaryOperator.js
@@ -0,0 +1,6 @@
+// even or odd
+let num = 8
+
+let result = num % 2 === 0 ? "Even" : "Odd"
+
+console.log(result)
---
+++ Basics/Conditional-Statements/switch.js
@@ -0,0 +1,19 @@
+// day of week
+let day = 3
+
+switch (day) {
+  case 1:
+    console.log("Monday")
+    break
+
+  case 2:
+    console.log("Tuesday")
+    break
+
+  case 3:
+    console.log("Wednesday")
+    break
+
+  default:
+    console.log("Invalid day")
+}
